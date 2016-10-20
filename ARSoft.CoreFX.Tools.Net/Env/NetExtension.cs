45c2ff15b66428c6719809df87cd735df6b8f55b
Administrator <Administrator@DESKTOP-8HDOB22>
2016-10-20 06:59:09 +0200

    Add project files.

---
+++ ARSoft.CoreFX.Tools.Net/Env/NetExtension.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace System.Net
+{
+
+    // Environment.OSVersion.Platform == PlatformID.Unix
+
+    public static class NetExtension
+    {
+
+        public static void Close(this TcpClient tcpClient)
+        {
+        }
+
+        public static void Close(this System.Threading.WaitHandle handle)
+        {
+        }
+    }
+}
---
+++ ARSoft.CoreFX.Tools.Net/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ARSoft.CoreFX.Tools.Net")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9c3c1c83-31b8-415c-bb60-edeeef2834ef")]
